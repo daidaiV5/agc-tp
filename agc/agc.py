b63084f4b7fd59b53e58f83a2f18d9d1ecfa8e54
daidaiV5 <62575671+daidaiV5@users.noreply.github.com>
2021-10-20 22:15:54 +0200

    last_commit

--- agc/agc.py
+++ agc/agc.py
@@ -18,18 +18,18 @@ import sys
 import os
 import gzip
 import statistics
+import operator
 from collections import Counter
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
 
-__author__ = "Your Name"
+__author__ = "DAI_EID"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["DAI_EID"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "DAI_EID"
 __status__ = "Developpement"
 
 
@@ -69,12 +69,38 @@ def get_arguments():
                         default="OTU.fasta", help="Output file")
     return parser.parse_args()
 
+
 def read_fasta(amplicon_file, minseqlen):
-    pass
+    with gzip.open(amplicon_file, "rt") as file:
+        dico= {}
+        prot_id = ""
+        for ligne in file:
+            if ligne.startswith(">"):
+                prot_id = ligne[1:].split()[0]
+                dico[prot_id] = ""
+            else:
+                dico[prot_id] += ligne.strip()
+        for i in dico:
+            sequence = dico[i]
+            if len(sequence) >= minseqlen:
+                yield sequence
 
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
-    pass
+    dico={}
+    list_seq=[]
+    for i in read_fasta(amplicon_file, minseqlen) :
+        list_seq.append(i)
+    list_index=set(list_seq)
+    for i in list_index:
+        dico[i]=list_seq.count(i)    
+    #print (dico)
+    #print(new_dico)
+    new_dico=dict(sorted(dico.items(), key = lambda x: x[1], reverse = True))
+    #print(new_dico)
+    for i,j in new_dico.items():
+        if j >= mincount:
+            yield[i, j]
 
 
 def get_unique(ids):
@@ -110,18 +136,122 @@ def get_identity(alignment_list):
             id_nu += 1
     return round(100.0 * id_nu / len(alignment_list[0]), 2)
 
-def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    #print(sequence)
+    for kmer in cut_kmer(sequence, kmer_size):
+        #print (kmer)
+        if kmer in kmer_dict:
+            kmer_dict[kmer]+=[id_seq]
+        else:
+            kmer_dict[kmer] = [id_seq]
+    #print (kmer_dict)
+    return (kmer_dict)
+
+
+def chimera_removal(amplicon_file , minseqlen ,mincount , chunk_size , kmer_size ):
+
+    list_sequences = []
+    list_occurences = []
+    for seq_occ in dereplication_fulllength(amplicon_file, minseqlen, mincount):
+        list_sequences.append(seq_occ[0])
+        list_occurences.append(seq_occ[1])
+    chunks = []
+    kmer_dict = {}
+    for id, seq in enumerate(list_sequences):
+        chunks.append(get_chunks(seq, chunk_size))
+        kmer_dict = get_unique_kmer(kmer_dict, seq, id, kmer_size)
+    parent = []
+    for chunk in chunks:
+        for seq2 in chunk:
+            parent.append(search_mates(kmer_dict, seq2, kmer_size))
+
+    parent_seq = common(parent[0], parent[1])
+    chim_id = []
+    chunk_list = [get_chunks(list_sequences[parent_seq[0]], chunk_size)]
+    chunk_list += [get_chunks(list_sequences[parent_seq[1]], chunk_size)]
+
+    for id, seq in enumerate(list_sequences):
+        if seq not in parent_seq:
+            chimera = get_chunks(seq, chunk_size)
+            matrix = [[] for chunk in range(len(chimera))]
+            for id2, chunk2 in enumerate(chimera):
+                for chunk in range(len(chunk_list)):
+                    matrix[id2].append(get_identity(nw.global_align(chunk2,
+                        chunk_list[chunk], gap_open=-1, gap_extend=-1, matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))))
+                #print (id2)
+            if detect_chimera(matrix):
+                chim_id.append(index)
+
+    for index, seq in enumerate(sequences):
+        if index not in chim_id:
+            yield [seq, occurences[index]]
+
+
+
+def detect_chimera(perc_identity_matrix):
+    list_sd=[]
+    list_proche=[]
+    for i in perc_identity_matrix:
+        std=round(statistics.stdev(i),1)
+        list_sd.append(std)
+        if i[0]>i[1]:
+            list_proche.append(1)
+        else :
+            list_proche.append(0)
+    moy= round(statistics.mean(list_sd),1)
+    if moy >5:
+        c1=list_proche.count(1)
+        c0=list_proche.count(0)
+        if c1 == 0 or c0 ==0 :
+            return False
+        else :
+           return True
+    else:
+        return False
+
+
+def search_mates(kmer_dict, sequence, kmer_size):
+    list_kmer = []
+    for kmer in cut_kmer(sequence, kmer_size):
+        list_kmer.append(kmer)
+        if kmer in kmer_dict:
+            list_kmer += kmer_dict[kmer]
+    best_mates = Counter(list_kmer).most_common(2)
+    return [seq_id for seq_id, count in best_mates]
+
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    #normalement il faut changer a ce niveau dereplication_fulllength et metre chimera_removal, mais vue que cette derniere n'a pas reussi le test on prefere garder dereplication_fulllength
+    list_otu=[]
+    list_occ = [seq for seq in dereplication_fulllength(amplicon_file, minseqlen, mincount)]
+    list_otu.append([list_occ[0][0], list_occ[0][1]])
+    for i in range(1,len(list_occ)):
+        for j in range(0,len(list_occ)):
+            #print(j)
+            align=nw.global_align(list_occ[i][0],list_occ[j][0], gap_open=-1, gap_extend=-1, matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))
+            align=list(align)
+            #print(align)
+            sim=get_identity(align)
+            #print(sim)
+            if (sim <97):
+                list_otu.append([list_occ[i][0], list_occ[i][1]])
+    return (list_otu)
+
+
 
 def fill(text, width=80):
     """Split text with a line return to respect fasta format"""
     return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
 
 def write_OTU(OTU_list, output_file):
-    pass
+    with open(output_file, "w") as f:
+        count = 1
+        for i, (seq, occ) in enumerate(OTU_list):
+            f.write(f">OTU_{count} occurrence:{occ}\n")
+            f.write(f"{fill(seq)}")
+            f.write("\n")
+            count += 1
 
 #==============================================================
 # Main program
@@ -132,8 +262,9 @@ def main():
     """
     # Get arguments
     args = get_arguments()
-    # Votre programme ici
-
+    # notre programme ici
+    chimerafree = abundance_greedy_clustering(args.amplicon_file, args.minseqlen, args.mincount, args.chunk_size, args.kmer_size)
+    print (chimerafree)
 
 if __name__ == '__main__':
     main()
